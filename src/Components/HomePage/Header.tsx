88b8670165d975a3c634bcb52f4e337dc2c90a95
pumbas600 <joshjeffers600@gmail.com>
2022-04-19 19:12:39 +1200

    Changed colour of header to teal

--- src/Components/HomePage/Header.tsx
+++ src/Components/HomePage/Header.tsx
@@ -6,15 +6,13 @@ interface Props {
 
 const Header:React.FC<Props> = (props) => {
     return (
-        <div className="header-group">
-            <div className="header-secondary">
-                <div className="navigation-buttons centred-body">
-                    <i onClick={() => props.toggleTheme()} className="btn nav-button fas fa-adjust"/>
-                    <a href="#about-me"  className="nav-button link-hover-effect">About Me</a>
-                    <a href="#education" className="nav-button link-hover-effect">Education</a>
-                    <a href="#projects"  className="nav-button link-hover-effect">Projects</a>
-                    <a href="#contact-me"  className="nav-button link-hover-effect">Contact Me</a>
-                </div>
+        <div className="w-full bg-teal-200 py-2 shadow-teal-200/50 shadow-sm">
+            <div className="flex flex-row justify-end centred-body items-center text-xl font-bold">
+                <i onClick={() => props.toggleTheme()} className="ml-5 fas fa-adjust"/>
+                <a href="#about-me" className="ml-5">About Me</a>
+                <a href="#technical-skills" className="ml-5">Skills</a>
+                <a href="#projects" className="ml-5">Projects</a>
+                <a href="#contact-me" className="ml-5">Contact Me</a>
             </div>
         </div>
     )
