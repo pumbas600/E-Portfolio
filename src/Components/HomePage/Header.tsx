af72bb42c084a84197d1056eb9248223ddca627e
pumbas600 <joshjeffers600@gmail.com>
2022-04-19 13:31:55 +1200

    Removed old primary header

--- src/Components/HomePage/Header.tsx
+++ src/Components/HomePage/Header.tsx
@@ -8,24 +8,6 @@ interface IProps {
 const Header:React.FC<IProps> = (props) => {
     return (
         <div className="header-group">
-            <div className="header-background">
-                <div className="header centred-body">
-                    <div className="profile-image">
-                        <img src={selfie} alt="Josh Jeffers"/>
-                    </div>
-                    <div className="header-text">
-                        <h1>Josh Jeffers</h1>
-                        <div className="profile-links">
-                            <a target="_blank" rel="noreferrer" href="https://github.com/pumbas600">
-                                <i className="fab fa-github"/>
-                            </a>
-                            <a href="mailto:joshjeffers600@gmail.com">
-                                <i className="far fa-envelope"/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div className="header-secondary">
                 <div className="navigation-buttons centred-body">
                     <i onClick={() => props.toggleTheme()} className="btn nav-button fas fa-adjust"/>
