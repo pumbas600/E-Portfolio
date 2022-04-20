7b2c1fef6bd8111a079d2ee7170c971c5b419a60
pumbas600 <joshjeffers600@gmail.com>
2022-04-21 10:54:58 +1200

    Added some styling to contact me

--- src/Components/HomePage/ContactMe.tsx
+++ src/Components/HomePage/ContactMe.tsx
@@ -9,17 +9,33 @@ const ContactMe: React.FC = () => {
 
     const [state, setState] = useState<State>({ message: '' });
 
-    function updateMessage() {
+    function updateMessage(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        setState({ message: e.target.value });
+    }
 
+    function sendMessage() {
+        setState({ message: '' });
     }
 
     return (
         <div>
             <Title name="Contact Me"/>
-            <div className="rounded-lg bg-gradient-to-r dark:from-gray-700 dark:to-slate-600 from-gray-800 to-slate-800 md:py-5 py-4 md:px-8 px-5">
-                <div className="mx-auto grid grid-cols-1 gap-1 w-1/2">
-                    <textarea value={state.message} className="rounded-lg h-24" />
-                    <button className="text-xl font-bold rounded-lg py-1 w-full text-gray-800 bg-teal-200 hover:bg-teal-300">
+            <div className="rounded-lg bg-gradient-to-r dark:from-gray-700 dark:to-slate-600 from-gray-800 to-slate-800
+                            md:py-5 py-4 px-10"
+            >
+                <div className="mx-auto grid grid-cols-1 gap-1 md:w-8/12 w-full text-sm text-gray-300">
+                    <textarea
+                        className="rounded-lg h-32 ring-1 ring-gray-300 bg-transparent p-2 outline-none focus:ring-2
+                                   focus:ring-teal-200 ring-inset"
+                        onChange={updateMessage}
+                        placeholder="Enter your message..."
+                        value={state.message}
+                    />
+                    <button
+                        className="text-xl font-bold rounded-lg py-1 w-full text-gray-800 bg-gradient-to-br
+                                   from-teal-200 to-teal-300 hover:from-teal-100 hover:to-teal-200"
+                        onClick={e => sendMessage()}
+                    >
                         Send
                     </button>
                 </div>
