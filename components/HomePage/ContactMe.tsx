aeb03570469fd99b38635279ef0670e3dbaad8c7
pumbas600 <joshjeffers600@gmail.com>
2022-11-06 16:18:27 +1300

    refactor: :recycle: Replace inline-svgs with font awesome icons

--- components/HomePage/ContactMe.tsx
+++ components/HomePage/ContactMe.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Title from '../Utils/Title';
 import LabelledInput from '../Utils/LabelledInput';
 import InputField from '../Utils/InputField';
 import { CSSTransition } from 'react-transition-group';
 import Card from '../Utils/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch, faEnvelopeCircleCheck, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 type SendingState = 'UNSENT' | 'SENDING' | 'SENT';
 
@@ -142,42 +144,15 @@ const ContactMe: React.FC = () => {
 			case 'UNSENT':
 				return (
 					<>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							fill="currentColor"
-							className="bi bi-send"
-							viewBox="0 0 16 16"
-						>
-							<path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-						</svg>
+						<FontAwesomeIcon icon={faPaperPlane} />
 						<div className="ml-2">Send</div>
 					</>
 				);
 			case 'SENDING':
 				return (
+					//"animate-spin -ml-1 mr-3 h-5 w-5 text-white"
 					<>
-						<svg
-							className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle
-								className="opacity-25"
-								cx="12"
-								cy="12"
-								r="10"
-								stroke="currentColor"
-								strokeWidth="4"
-							></circle>
-							<path
-								className="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							></path>
-						</svg>
+						<FontAwesomeIcon icon={faCircleNotch} spin />
 						<div className="ml-2">Sending</div>
 					</>
 				);
@@ -211,17 +186,7 @@ const ContactMe: React.FC = () => {
 				>
 					<div className={`w-full py-5 px-10 ${SEND_BUTTON_STYLE}`}>
 						<div className="text-2xl font-bold flex flex-row items-center">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="16"
-								height="16"
-								fill="currentColor"
-								className="bi bi-send-check-fill"
-								viewBox="0 0 16 16"
-							>
-								<path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 1.59 2.498C8 14 8 13 8 12.5a4.5 4.5 0 0 1 5.026-4.47L15.964.686Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-								<path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
-							</svg>
+							<FontAwesomeIcon icon={faEnvelopeCircleCheck} />
 							<div className="ml-2">Sent!</div>
 						</div>
 						<div className="text-lg">Thank you for your message! :)</div>
