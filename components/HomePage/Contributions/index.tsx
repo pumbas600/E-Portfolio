51fe56d0f366f59196f238c8c64cb232ba87aaa5
pumbas600 <joshjeffers600@gmail.com>
2023-09-11 01:09:37 +1200

    feat: :sparkles:  Adjust contributions graph based on whether its being viewed on mobile or not

--- components/HomePage/Contributions/index.tsx
+++ components/HomePage/Contributions/index.tsx
@@ -3,12 +3,29 @@ import Title from '../../Title';
 import { Theme } from '../../../pages';
 import Card from '../../Card';
 import AnimatedLink from '../../Links/AnimatedLink';
+import useIsMobile from '../../../hooks/useIsMobile';
+import { useMemo } from 'react';
 
 interface ContributionsProps {
 	theme: Theme;
 }
 
+interface ApiOptions {
+	width: number;
+	height: number;
+	days: number;
+}
+
 export default function Contributions({ theme }: ContributionsProps) {
+	const isMobile = useIsMobile();
+
+	const { width, height, days } = useMemo<ApiOptions>(() => {
+		if (isMobile) {
+			return { width: 600, height: 400, days: 15 };
+		}
+		return { width: 1000, height: 400, days: 30 };
+	}, [isMobile]);
+
 	const colour = theme === 'dark' ? '99F6E4' : '0284C7';
 
 	return (
@@ -16,11 +33,11 @@ export default function Contributions({ theme }: ContributionsProps) {
 			<Title name="Contributions" />
 			<Card>
 				<Image
-					className="-ml-2"
-					width={1000}
-					height={400}
-					src={`https://github.pumbas.net/api/contributions/pumbas600?width=1000&height=400&colour=${colour}&dotColour=FFFFFF`}
-					alt="My GitHub contributions over the past 30 days"
+					className="-ml-1"
+					width={width}
+					height={height}
+					src={`https://github.pumbas.net/api/contributions/pumbas600?width=${width}&height=${height}&days=${days}&colour=${colour}&dotColour=FFFFFF`}
+					alt={`My GitHub contributions over the past ${days} days`}
 				/>
 				<div>
 					This graph has been dynamically generated from my recent GitHub contributions using my{' '}
