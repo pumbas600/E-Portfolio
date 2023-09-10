eb13c998e8ede7985caa351d9ef9e95cc6da44b4
pumbas600 <joshjeffers600@gmail.com>
2023-09-10 23:11:41 +1200

    feat: :sparkles: Link contributions to github contributions project

--- components/HomePage/Contributions/index.tsx
+++ components/HomePage/Contributions/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Title from '../../Title';
 import { Theme } from '../../../pages';
+import Card from '../../Card';
+import AnimatedLink from '../../Links/AnimatedLink';
 
 interface ContributionsProps {
 	theme: Theme;
@@ -12,13 +14,19 @@ export default function Contributions({ theme }: ContributionsProps) {
 	return (
 		<section>
 			<Title name="Contributions" />
-			<Image
-				className="dark:border-0 border border-gray-200 justify-between bg-gradient-to-r rounded-lg dark:from-gray-700 dark:to-slate-600 shadow-xl"
-				width={1000}
-				height={400}
-				src={`https://github.pumbas.net/api/contributions/pumbas600?width=1000&height=400&colour=${colour}&dotColour=FFFFFF`}
-				alt="My GitHub contributions over the past 30 days"
-			/>
+			<Card>
+				<Image
+					className="-ml-2"
+					width={1000}
+					height={400}
+					src={`https://github.pumbas.net/api/contributions/pumbas600?width=1000&height=400&colour=${colour}&dotColour=FFFFFF`}
+					alt="My GitHub contributions over the past 30 days"
+				/>
+				<p className="">
+					This graph has been dynamically generated from my recent GitHub contributions using my{' '}
+					<AnimatedLink href="#github-contributions">GitHub Contributions</AnimatedLink> API.
+				</p>
+			</Card>
 		</section>
 	);
 }
--- components/HomePage/ProjectHighlights/index.tsx
+++ components/HomePage/ProjectHighlights/index.tsx
@@ -38,13 +38,13 @@ export default function ProjectHighlights() {
 					technologies={['nextjs', 'react', 'typescript', 'tailwindcss']}
 				/>
 				<ProjectCard
-					name="Github Contributions"
+					name="GitHub Contributions"
 					description={
 						<>
-							GitHub Contributions is a simple API I made that can be used to generated dynamic graphs of
-							your recent GitHub contributions that can be used as image urls in markdown files. The API
-							supports a number of query parameters which allow the style of the graph to be completely
-							customised by users.
+							GitHub Contributions is a simple API I made that can generated dynamic graphs of your recent
+							GitHub contributions which can be used as image urls in markdown files. The API supports a
+							number of query parameters which allow the style of the graph to be completely customised to
+							get the style desired by users.
 							<br />
 							<br />
 							It utilises Next.JS for the API, React for rendering the contribution graph, Firestore for
