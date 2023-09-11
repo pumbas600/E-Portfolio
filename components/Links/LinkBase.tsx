8bf6bb8ad7d807ed3fd48332557402f00b992f3d
pumbas600 <joshjeffers600@gmail.com>
2023-09-11 15:10:58 +1200

    config: :wrench: Add prettierrc.json file

--- components/Links/LinkBase.tsx
+++ components/Links/LinkBase.tsx
@@ -5,28 +5,32 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 
 interface LinkBaseProps {
-	href: string;
-	children?: ReactNode;
+  href: string;
+  children?: ReactNode;
 }
 
 export default function LinkBase({ href, children }: LinkBaseProps) {
-	const isInternal = !href.startsWith('http');
-	const isAnchor = href.startsWith('#');
+  const isInternal = !href.startsWith('http');
+  const isAnchor = href.startsWith('#');
 
-	function smoothScrollToAnchor() {
-		const element = document.getElementById(href.substring(1));
-		element?.scrollIntoView();
-	}
+  function smoothScrollToAnchor() {
+    const element = document.getElementById(href.substring(1));
+    element?.scrollIntoView();
+  }
 
-	if (isAnchor) {
-		return <div onClick={smoothScrollToAnchor}>{children}</div>;
-	}
+  if (isAnchor) {
+    return (
+      <div className="cursor-pointer" onClick={smoothScrollToAnchor}>
+        {children}
+      </div>
+    );
+  }
 
-	return (
-		<Link target={isInternal ? undefined : '_blank'} rel="noreferrer" href={href} className="inline-block">
-			{children}
-		</Link>
-	);
+  return (
+    <Link target={isInternal ? undefined : '_blank'} rel="noreferrer" href={href} className="inline-block">
+      {children}
+    </Link>
+  );
 }
 
 export type { LinkBaseProps };
