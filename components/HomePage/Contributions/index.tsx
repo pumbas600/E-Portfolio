791e6acfa21ed6a6095e6ee4f34026dd556a4199
pumbas600 <joshjeffers600@gmail.com>
2023-10-29 00:19:55 +1300

    refactor: :recycle: Update API options being used
    It was previously using the now deprecated `width` and `height` options. As these are now dynamically determined based on the number of days, we don't need to manually specify them.

--- components/HomePage/Contributions/index.tsx
+++ components/HomePage/Contributions/index.tsx
@@ -3,28 +3,15 @@ import { Theme } from '../../../pages';
 import Card from '../../Card';
 import AnimatedLink from '../../Links/AnimatedLink';
 import useIsMobile from '../../../hooks/useIsMobile';
-import { useMemo } from 'react';
 
 interface ContributionsProps {
   theme: Theme;
 }
 
-interface ApiOptions {
-  width: number;
-  height: number;
-  days: number;
-}
-
 export default function Contributions({ theme }: ContributionsProps) {
   const isMobile = useIsMobile();
 
-  const { width, height, days } = useMemo<ApiOptions>(() => {
-    if (isMobile) {
-      return { width: 600, height: 400, days: 15 };
-    }
-    return { width: 1000, height: 400, days: 30 };
-  }, [isMobile]);
-
+  const days = isMobile ? 15 : 30;
   const colour = theme === 'dark' ? '99F6E4' : '0284C7';
 
   return (
@@ -33,9 +20,7 @@ export default function Contributions({ theme }: ContributionsProps) {
       <Card>
         <img
           className="-ml-1"
-          width={width}
-          height={height}
-          src={`https://github.pumbas.net/api/contributions/pumbas600?width=${width}&height=${height}&days=${days}&colour=${colour}&dotColour=FFFFFF`}
+          src={`https://github.pumbas.net/api/contributions/pumbas600?days=${days}&colour=${colour}&dotColour=FFFFFF`}
           alt={`My GitHub contributions over the past ${days} days`}
         />
         <div>
