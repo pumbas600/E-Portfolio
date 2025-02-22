bf3cf5c91a2a12826eb7d074b15f31ae99124135
pumbas600 <joshjeffers600@gmail.com>
2025-02-22 21:54:48 +1300

    chore: :bricks: adjust wording of portfolio card

--- src/components/Card/PortfolioCard.tsx
+++ src/components/Card/PortfolioCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Badges } from '../Badges';
+import LinkBase from '../Links/LinkBase';
 import ProjectCard from './ProjectCard';
 
 export default function PortfolioCard() {
@@ -8,11 +9,14 @@ export default function PortfolioCard() {
     <ProjectCard
       name="Portfolio"
       description={
-        <>
-          This digital portfolio was created as a way for me to develop my frontend knowledge and expose myself to
-          React, and modern CSS features. I leaveraged Next.JS for its Static Site Generation, allowing the HTML to be
-          generated at build time.
-        </>
+        <p>
+          This is the second iteration of my digital portfolio. It's primary purpose has been to exist as a playground
+          where I can develop my frontend knowledge. It has allowed me to expose myself to React, and modern CSS
+          features. The entire website is built using{' '}
+          <LinkBase href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_grid_layout">CSS grids</LinkBase> and
+          fluid scales to dynamically adjust spacing and typography between mobile and desktop layouts. It also
+          leverages Next.JS' Static Site Generation features to generate the HTML for the website at build time.
+        </p>
       }
       date="February 2025"
       githubUrl="https://github.com/pumbas600/E-Portfolio"
