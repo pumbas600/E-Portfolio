c19cba32db87d6b2b2bb3bb6aee15a474392f84c
Josh Jeffers <josh@letterboxd.com>
2025-05-19 21:23:10 +1200

    style: :lipstick: add transition styles to badge

--- src/components/Badges/TechnologyBadge.tsx
+++ src/components/Badges/TechnologyBadge.tsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import { BadgeProps, BadgeWrapper } from './Badge';
 
 const TechnologyBadgeWrapper = styled(BadgeWrapper)<{ $hoverBackground?: string; $hoverColor?: string }>`
+  transition-property: color, background-color;
+  transition-duration: 150ms;
+  transition-timing-function: ease-in-out;
   background-color: var(--primary-background-color);
   padding-inline: var(--space-1);
   padding-block: var(--space-0_5);
