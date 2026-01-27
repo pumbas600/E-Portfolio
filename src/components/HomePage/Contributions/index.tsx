f5a7e0824c8c24ad49993dbe5318eda82bafe56c
pumbas600 <joshjeffers600@gmail.com>
2026-01-27 23:06:24 +1300

    feat: make contributions a server component using a <picture>

--- src/components/HomePage/Contributions/index.tsx
+++ src/components/HomePage/Contributions/index.tsx
@@ -1,32 +1,26 @@
-'use client';
-
-import useIsMobile from '../../../hooks/useIsMobile';
 import Card from '../../Card';
 import LinkBase from '../../Links/LinkBase';
 import { SecondaryText } from '../../Typography';
-import styled from 'styled-components';
-
-const ContributionsGraph = styled.img`
-  width: 100%;
-  grid-column-start: margin-start;
-  grid-column-end: margin-end;
-
-  padding-inline-start: var(--space-2_5);
-  padding-inline-end: var(--space-3);
-`;
+import styles from './Contributions.module.css';
 
 export default function Contributions() {
-  const isMobile = useIsMobile();
-
-  const days = isMobile ? 15 : 30;
   const colour = '63fd8b'; // Secondary accent colour
 
   return (
     <Card>
-      <ContributionsGraph
-        src={`https://github.pumbas.net/api/contributions/pumbas600?days=${days}&colour=${colour}&dotColour=FFFFFF`}
-        alt={`My GitHub contributions over the past ${days} days`}
-      />
+      <picture>
+        <source
+          srcSet={`https://github.pumbas.net/api/contributions/pumbas600?days=15&colour=${colour}&dotColour=FFFFFF`}
+          media="(width <= 40rem)"
+        />
+        <img
+          src={`https://github.pumbas.net/api/contributions/pumbas600?days=30&colour=${colour}&dotColour=FFFFFF`}
+          alt="My recent GitHub contributions"
+          className={styles.graph}
+          loading="lazy"
+        />
+      </picture>
+
       <SecondaryText>
         This graph has been dynamically generated from my recent GitHub contributions using my{' '}
         <LinkBase href="#github-contributions">GitHub Contributions API</LinkBase> ✨.
